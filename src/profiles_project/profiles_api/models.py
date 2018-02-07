2ced0a7918e0cba15c60615dce4ac60bbc2fdf60
SukhnandanMalhotra <sukh.malhotra@gmail.com>
2018-02-08 00:34:56 +0530

    Added model for profile field model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -51,3 +51,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Converts object to a string."""
 
         return self.email
+
+class ProfileFeedItem(models.Model):
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return self.status_text
